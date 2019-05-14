fd4e0919b3bfde40c20420a75389fe34bf6b0c84
hschafer2017 <hschafer@umich.edu>
2019-05-13 20:34:52 -0400

    brought photo grid to landing page with slight styling

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import GifButton from './Button.js'
+import GifGrid from './Grid.js'
 
 
 ReactDOM.render(<GifButton />, document.getElementById('gif-button'));
+ReactDOM.render(<GifGrid />, document.getElementById('gif-images'));
