c51d9bd9e6ff0315ac47fb6f4883d7ecd239ed6c
hschafer2017 <hschafer@umich.edu>
2019-05-14 21:47:35 -0400

    exported XML Request on state change to load initial GIF links to component

--- src/apiUtils.js
+++ src/apiUtils.js
@@ -1,25 +1,69 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
 let randomWords = require('random-words');
 let request = new XMLHttpRequest();
 
+var tileData = []
+
+
 function displayGIFNicely(apiData) {
-    let newData = JSON.parse(apiData);
-    
-    let data = newData.data[0].embed_url;
-    document.getElementById("gif-image").innerHTML =
-        // "&lt;embed src='" + data +  "'&gt;";
-        "<embed src='" + data +  "'>";
-        
+    let apiResponseData = JSON.parse(apiData);
+
+    Array(9).fill().map((_,i) => 
+        tileData.push({
+            img: `${apiResponseData.data[i].embed_url}`,
+            cols: 2,
+        })
+    )
+    return tileData
 }
 
-request.onreadystatechange = function() {
-    if (this.readyState === 4 && this.status === 200) {
-        displayGIFNicely(this.responseText);
-        
+
+class GifGrid extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          tileData: []
+        }
     }
-    else if (this.readyState === 4 && this.status === 404) {
-        
+
+    componentDidMount() {
+    let request = new XMLHttpRequest();
+    var self = this;
+
+        request.onreadystatechange = function(e){
+            if (this.readyState === 4 && this.status === 200) {
+                self.setState({
+                tileData : displayGIFNicely(this.responseText)
+                });
+            }
+            else if (this.readyState === 4 && this.status === 404) {
+            }
+        }
+
+    let search = randomWords({exactly:1, wordsPerString:2});
+    request.open("GET", "https://giveagif.herokuapp.com/gifs/" + search);
+    request.send();
+
     }
-};
+
+    render() {
+        let tileDataLoaded = this.state.tileData.length > 0;
+        console.log(tileDataLoaded)
+        return(
+            tileDataLoaded ?
+            <ul>
+            {this.state.tileData.map(
+                tile => {
+                    return <li>{ tile.img }</li>;
+                })}
+            </ul>
+            :
+            <div>Your Gifs Are Loading...</div>
+        );
+      }
+  }
 
 
 // API KEY USING RANDOM GENERATOR FOR SEARCHED GIFS
@@ -29,3 +73,8 @@ export function GIF_api() {
     request.send();
     return false;
 }
+
+ReactDOM.render(
+    <GifGrid />,
+    document.getElementById('component-testing')
+  );
