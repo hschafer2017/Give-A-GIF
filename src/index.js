acd6a13deb00a2f1ac06708330ceb2e5a28f3d9a
hschafer2017 <hschafer@umich.edu>
2019-05-21 22:14:08 -0400

    added card to show query string words and instructions to for more gifs - WIP

--- src/index.js
+++ src/index.js
@@ -4,9 +4,10 @@ import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import GifButton from './Button.js'
 import GifGrid from './apiUtils.js'
-// import ControlledExpansionPanels from './Expansion.js'
+import IntroCard from './IntroCard.js'
+
 
 
 ReactDOM.render(<GifButton />, document.getElementById('gif-button'));
 ReactDOM.render(<GifGrid />, document.getElementById('gif-images'));
-// ReactDOM.render(<ControlledExpansionPanels/>, document.getElementById('gif-images'));
+ReactDOM.render(<IntroCard />, document.getElementById('query-card'));
