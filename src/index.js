44b61101b0abf986c5ea29728a5ad43959376126
hschafer2017 <hschafer@umich.edu>
2019-05-21 22:20:24 -0400

    Revert "added card to show query string words and instructions to for more gifs - WIP"
    This reverts commit acd6a13deb00a2f1ac06708330ceb2e5a28f3d9a.

--- src/index.js
+++ src/index.js
@@ -4,10 +4,9 @@ import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import GifButton from './Button.js'
 import GifGrid from './apiUtils.js'
-import IntroCard from './IntroCard.js'
-
+// import ControlledExpansionPanels from './Expansion.js'
 
 
 ReactDOM.render(<GifButton />, document.getElementById('gif-button'));
 ReactDOM.render(<GifGrid />, document.getElementById('gif-images'));
-ReactDOM.render(<IntroCard />, document.getElementById('query-card'));
+// ReactDOM.render(<ControlledExpansionPanels/>, document.getElementById('gif-images'));
