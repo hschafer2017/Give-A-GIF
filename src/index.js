5c8b7f3e23446546721ce19c66dc09a2c46208dc
hschafer2017 <hschafer@umich.edu>
2019-05-28 20:55:58 -0400

    added and styled helper expansion card with positioning

--- src/index.js
+++ src/index.js
@@ -2,12 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
-import GifButton from './Button.js'
 import GifGrid from './apiUtils.js'
 import IntroCard from './IntroCard.js'
+import ControlledExpansionPanels from './Expansion.js'
 
 
-
-ReactDOM.render(<GifButton />, document.getElementById('gif-button'));
 ReactDOM.render(<GifGrid />, document.getElementById('gif-images'));
 ReactDOM.render(<IntroCard />, document.getElementById('query-card'));
+ReactDOM.render(<ControlledExpansionPanels/>, document.getElementById('help-panel'));
