f5cd1c2e80ea1c586c73ab2348bc1ac7bdbab5a3
hschafer2017 <hschafer@umich.edu>
2019-05-30 23:09:17 -0400

    added simple give a gif app bar

--- src/index.js
+++ src/index.js
@@ -5,8 +5,10 @@ import './index.css';
 import GifGrid from './apiUtils.js'
 import IntroCard from './IntroCard.js'
 import ControlledExpansionPanels from './Expansion.js'
+import GifNavBar from './NavBar.js'
 
 
+ReactDOM.render(<GifNavBar/>, document.getElementById('navbar'));
 ReactDOM.render(<GifGrid />, document.getElementById('gif-images'));
 ReactDOM.render(<IntroCard />, document.getElementById('query-card'));
 ReactDOM.render(<ControlledExpansionPanels/>, document.getElementById('help-panel'));
