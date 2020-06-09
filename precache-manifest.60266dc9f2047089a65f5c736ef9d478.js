cce54df877b8246d0abd9725b3640af9d19fce78
hschafer2017 <hschafer@umich.edu>
2020-06-09 23:07:12 +0100

    Updates

---
+++ precache-manifest.60266dc9f2047089a65f5c736ef9d478.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d680b05a77acfe9ef42c",
+    "url": "/Give-A-GIF/static/css/main.8450e81c.chunk.css"
+  },
+  {
+    "revision": "d680b05a77acfe9ef42c",
+    "url": "/Give-A-GIF/static/js/main.d680b05a.chunk.js"
+  },
+  {
+    "revision": "ecfe1951a0a176a8cad4",
+    "url": "/Give-A-GIF/static/css/1.da1632bf.chunk.css"
+  },
+  {
+    "revision": "ecfe1951a0a176a8cad4",
+    "url": "/Give-A-GIF/static/js/1.ecfe1951.chunk.js"
+  },
+  {
+    "revision": "7acd62cd3880a3aafaf5",
+    "url": "/Give-A-GIF/static/js/runtime~main.7acd62cd.js"
+  },
+  {
+    "revision": "c65dabae121bb64ce741405340554f50",
+    "url": "/Give-A-GIF/index.html"
+  }
+];
